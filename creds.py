9947e00d594c13822cad251122d79cddf87dd4f1
Rishmika <111166308+Rishmika@users.noreply.github.com>
2023-01-01 20:09:40 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,10 +5,10 @@ import os
 
 
 class Credentials:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")  # from @botfather
-    API_ID = int(os.environ.get("API_ID"))  # from https://my.telegram.org/apps
-    API_HASH = os.environ.get("API_HASH")  # from https://my.telegram.org/apps
+    BOT_TOKEN = os.environ.get("5985869623:AAEYkxV3pTAcedqt7EyAcmiat5rk2axiKRc")  # from @botfather
+    API_ID = int(os.environ.get("14055090"))  # from https://my.telegram.org/apps
+    API_HASH = os.environ.get("a46f7b439d0afa45b7a69fc450f754e9")  # from https://my.telegram.org/apps
     # Banned Unwanted Members..
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS","").split())
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS","5716680911").split())
 
 # Okay 🤣
